510b6f74797c983d10e1b069223df1c8829c6372
Prathmesh Takalkar <takalkarprathmesh@gmail.com>
2024-03-20 11:32:59 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,130 @@
+import bcrypt from "bcrypt";
+import bodyParser from "body-parser";
+import express from "express";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+const saltRounds = 11;
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "feedbackhub",
+  password: "undertaker",
+  port: 5432,
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// Get Methods
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.get("/logIn", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/reviews", async (req, res) => {
+  try {
+    const result = await db.query("SELECT * FROM whatsay;");
+    const feed = result.rows;
+    res.render("reviews.ejs", {
+      review: feed,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/feedback", (req, res) => {
+  res.render("feedback.ejs");
+});
+
+// Post Methods
+
+app.post("/register", async (req, res) => {
+  const name = req.body.username;
+  const email = req.body.email;
+  const pass = req.body.password;
+
+  try {
+    const checkResults = await db.query(
+      "SELECT * FROM users where email = $1",
+      [email]
+    );
+
+    if (checkResults.rows.length > 0) {
+      res.send("Email Already Exists, Try Logging In!!");
+    } else {
+      // Hashing
+      bcrypt.hash(pass, saltRounds, async (err, hash) => {
+        if (err) {
+          console.log(err);
+        } else {
+          const results = await db.query(
+            "INSERT INTO users (username , email , password) VALUES ($1 , $2 , $3)",
+            [name, email, hash]
+          );
+          res.render("home.ejs");
+        }
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  try {
+    const result = await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+    if (result.rows.length > 0) {
+      const user = result.rows[0];
+      const storedpassword = user.password;
+
+      // Unhashing
+      bcrypt.compare(password, storedpassword, async (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          if (result) {
+            res.render("home.ejs");
+          } else {
+            res.send("Incorrect Password!");
+          }
+        }
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/feedback", async (req, res) => {
+  const name = req.body.username;
+  const review = req.body.review;
+  try {
+    const result = await db.query(
+      "INSERT INTO whatsay (username , feedback) VALUES ($1 ,$2)",
+      [name, review]
+    );
+    res.render("feedback.ejs");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
---
+++ readme.md
@@ -0,0 +1,24 @@
+# Feedback Hub
+
+Welcome to Feedback Hub! This is a dynamic application designed to showcase my portfolio , collect feedback from users and showcase reviews. Feedback Hub provides a platform for users to leave feedback, view my basic info with contact info, and explore reviews.
+
+## Features
+
+- **User Authentication:** Users can register and login securely using hashed and salted passwords.
+- **Feedback Submission:** Users can provide feedback, which is stored securely in the PostgreSQL database.
+- **Contact Information:** Users can view contact information for the project owner.
+- **Reviews:** Users can see all feedback received by the project owner.
+
+## Tech Stack
+
+- **Backend:** Node.js, Express.js
+- **Frontend:** EJS, Bootstrap
+- **Database:** PostgreSQL
+
+## Prototype
+
+You can view the flowchart and prototype of the FeedbackHub application in the folders
+
+## Contributing
+
+Contributions are welcome! If you'd like to contribute to this project, please fork the repository and submit a pull request with your changes.
